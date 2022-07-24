7d807c7bb3b8e7adb3c89b604866ef31525bb0a4
ddilarakarakas <karakasdilara1@gmail.com>
2022-07-24 15:24:20 +0300

    Project structure added
    Spring boot project initialized

---
+++ src/main/java/com/example/productlicensecontroller/ProductLicenseControllerApplication.java
@@ -0,0 +1,13 @@
+package com.example.productlicensecontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductLicenseControllerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductLicenseControllerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/productlicensecontroller/ProductLicenseControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.productlicensecontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductLicenseControllerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
